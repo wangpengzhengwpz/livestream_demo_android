cae3170538fe62e014938bccf0738748165b992d
wangpengzhengwpz <2061743907@qq.com>
2017-04-12 20:02:23 +0800

    用户注册

--- app/src/main/java/cn/ucai/live/ui/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/RegisterActivity.java
@@ -8,76 +8,174 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import cn.ucai.live.R;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.exceptions.HyphenateException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.live.I;
+import cn.ucai.live.R;
+import cn.ucai.live.data.model.IUserModel;
+import cn.ucai.live.data.model.OnCompleteListener;
+import cn.ucai.live.data.model.UserModel;
+import cn.ucai.live.utils.CommonUtils;
+import cn.ucai.live.utils.L;
+import cn.ucai.live.utils.MD5;
+import cn.ucai.live.utils.MFGT;
+import cn.ucai.live.utils.Result;
+import cn.ucai.live.utils.ResultUtils;
 
 public class RegisterActivity extends BaseActivity {
-
+    private static final String TAG = "RegisterActivity";
     @BindView(R.id.email)
-    EditText username;
+    EditText mEtUsername;
     @BindView(R.id.password)
-    EditText password;
+    EditText mEtPassword;
     @BindView(R.id.register)
     Button register;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.nickname)
+    EditText mEtNickname;
+    @BindView(R.id.com_password)
+    EditText mEtComPassword;
+    String username, nickname, password;
+    ProgressDialog pd;
+    IUserModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        model = new UserModel();
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(TextUtils.isEmpty(username.getText()) || TextUtils.isEmpty(password.getText())){
-                    showToast("用户名和密码不能为空");
-                    return;
-                }
-                final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
-                pd.setMessage("正在注册...");
-                pd.setCanceledOnTouchOutside(false);
-                pd.show();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            EMClient.getInstance().createAccount(username.getText().toString(), password.getText().toString());
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    pd.dismiss();
-                                    showToast("注册成功");
-                                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                                    finish();
+    @OnClick(R.id.register)
+    public void registerAppServer() {
+        if (checkInput()) {
+            showDialog();
+            L.e(TAG, "password=" + MD5.getMessageDigest(password));
+            model.register(RegisterActivity.this, username, nickname, MD5.getMessageDigest(password),
+                    new OnCompleteListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            L.e(TAG, "s=" + s);
+                            boolean success = false;
+                            if (s != null) {
+                                Result result = ResultUtils.getResultFromJson(s, String.class);
+                                if (result != null) {
+                                    if (result.isRetMsg()) {
+                                        success = true;
+                                        registerEMServer();
+                                    } else if (result.getRetCode() == I.MSG_REGISTER_USERNAME_EXISTS) {
+                                        CommonUtils.showShortToast(R.string.User_already_exists);
+                                    } else {
+                                        CommonUtils.showShortToast(R.string.Registration_failed);
+                                    }
                                 }
-                            });
-                        } catch (final HyphenateException e) {
-                            e.printStackTrace();
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    pd.dismiss();
-                                    showLongToast("注册失败：" + e.getMessage());
-                                }
-                            });
+                            }
+                            if (!success) {
+                                pd.dismiss();
+                            }
                         }
-                    }
-                }).start();
+
+                        @Override
+                        public void onError(String error) {
+                            L.e(TAG, "error" + error);
+                            pd.dismiss();
+                            CommonUtils.showShortToast(R.string.Registration_failed);
+                        }
+                    });
+        }
+    }
+
+    private void registerEMServer() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    EMClient.getInstance().createAccount(username, MD5.getMessageDigest(password));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pd.dismiss();
+                            showToast("注册成功");
+//                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+//                            finish();
+                            MFGT.gotoLogin(RegisterActivity.this);
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    unregister();
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pd.dismiss();
+                            showLongToast("注册失败：" + e.getMessage());
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void unregister() {
+        model.unregister(RegisterActivity.this, username, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String result) {
+                L.e(TAG, "result=" + result);
+            }
+
+            @Override
+            public void onError(String error) {
 
             }
         });
+    }
+
+    private void showDialog() {
+        pd = new ProgressDialog(RegisterActivity.this);
+        pd.setMessage("正在注册...");
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+    }
+
+    public boolean checkInput() {
+        username = mEtUsername.getText().toString().trim();
+        nickname = mEtNickname.getText().toString().trim();
+        password = mEtPassword.getText().toString().trim();
+        String confirm_pwd = mEtComPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, getResources().getString(R.string.User_name_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            mEtUsername.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(nickname)) {
+            Toast.makeText(this, getResources().getString(R.string.toast_nick_not_isnull), Toast.LENGTH_SHORT).show();
+            mEtNickname.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, getResources().getString(R.string.Password_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            mEtPassword.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(confirm_pwd)) {
+            Toast.makeText(this, getResources().getString(R.string.Confirm_password_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            mEtComPassword.requestFocus();
+            return false;
+        } else if (!password.equals(confirm_pwd)) {
+            Toast.makeText(this, getResources().getString(R.string.Two_input_password), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/cn/ucai/live/utils/MFGT.java
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -1,14 +1,10 @@
 package cn.ucai.live.utils;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 
-import com.hyphenate.easeui.domain.User;
-
-import cn.ucai.live.I;
 import cn.ucai.live.R;
-import cn.ucai.live.ui.activity.MainActivity;
+import cn.ucai.live.ui.activity.LoginActivity;
 
 
 /**
@@ -23,15 +19,21 @@ public class MFGT {
 
     public static void startActivity(Activity activity, Class cls) {
         activity.startActivity(new Intent(activity, cls));
-        activity.overridePendingTransition(R.anim.push_bottom_in, R.anim.push_bottom_out);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
     }
 
-    public static void gotoMain(Activity activity) {
-        startActivity(activity, MainActivity.class);
+    public static void startActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
     }
 
     private static void startActivityForResult(Activity activity, Intent intent, int requestCode) {
         activity.startActivityForResult(intent, requestCode);
         activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void gotoLogin(Activity activity) {
+        startActivity(activity, new Intent(activity, LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 }
