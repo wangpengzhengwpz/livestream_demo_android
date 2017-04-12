b20e37e8e6595458111c5d191bb0c2b0501e68aa
wangpengzhengwpz <2061743907@qq.com>
2017-04-12 18:33:18 +0800

    添加数据Model层的接口和实现

---
+++ app/src/main/java/cn/ucai/live/data/model/OnCompleteListener.java
@@ -0,0 +1,11 @@
+package cn.ucai.live.data.model;
+
+
+import cn.ucai.live.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/3/29.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+}
