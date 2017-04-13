284e3fb76b0eb13f13b5c8437e72567a96ae9565
wangpengzhengwpz <2061743907@qq.com>
2017-04-13 17:43:03 +0800

    添加直播服务器接口定义

---
+++ app/src/main/java/cn/ucai/live/data/restapi/LivveService.java
@@ -0,0 +1,18 @@
+package cn.ucai.live.data.restapi;
+
+import cn.ucai.live.I;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by w on 2017/4/13.
+ */
+
+public interface LivveService {
+    @GET("live/getAllGifts")
+    Call<String> getAllGifts();
+
+    @GET("findUserByUserName")
+    Call<String> loadUserInfo(@Query(I.User.USER_NAME) String username);
+}
