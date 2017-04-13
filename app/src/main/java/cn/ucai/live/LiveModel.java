d48465b62ed727a8f87bee5c62157595f52b53b5
wangpengzhengwpz <2061743907@qq.com>
2017-04-13 13:09:08 +0800

    添加Model文件并删除不需要的方法

---
+++ app/src/main/java/cn/ucai/live/LiveModel.java
@@ -0,0 +1,26 @@
+package cn.ucai.live;
+
+import android.content.Context;
+
+import cn.ucai.live.utils.PreferenceManager;
+
+public class LiveModel {
+    protected Context context = null;
+
+    public LiveModel(Context ctx){
+        context = ctx;
+        PreferenceManager.init(context);
+    }
+    
+    /**
+     * save current username
+     * @param username
+     */
+    public void setCurrentUserName(String username){
+        PreferenceManager.getInstance().setCurrentUserName(username);
+    }
+
+    public String getCurrentUsernName(){
+        return PreferenceManager.getInstance().getCurrentUsername();
+    }
+}
