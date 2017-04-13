3b378286cb2dd75088a43aa59dab3979e406cd09
wangpengzhengwpz <2061743907@qq.com>
2017-04-13 14:30:55 +0800

    修改LiveApplication

--- app/src/main/java/cn/ucai/live/LiveApplication.java
+++ app/src/main/java/cn/ucai/live/LiveApplication.java
@@ -2,6 +2,7 @@ package cn.ucai.live;
 
 import android.app.Application;
 import android.content.Intent;
+
 import cn.ucai.live.ui.activity.MainActivity;
 import cn.ucai.live.utils.PreferenceManager;
 
@@ -14,53 +15,25 @@ import com.ucloud.ulive.UStreamingContext;
 /**
  * Created by wei on 2016/5/27.
  */
-public class LiveApplication extends Application{
+public class LiveApplication extends Application {
 
-  private static LiveApplication instance;
-
-
-  @Override public void onCreate() {
-    super.onCreate();
-    instance = this;
+    private static LiveApplication instance;
 
 
-    initChatSdk();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
 
-    //UEasyStreaming.initStreaming("publish3-key");
+        LiveHelper.getInstance().init(this);
 
-    UStreamingContext.init(getApplicationContext(), "publish3-key");
-  }
+        //UEasyStreaming.initStreaming("publish3-key");
 
-  public static LiveApplication getInstance(){
-    return instance;
-  }
+        UStreamingContext.init(getApplicationContext(), "publish3-key");
+    }
 
-  private void initChatSdk(){
-    //EMOptions options = new EMOptions();
-    //options.enableDNSConfig(false);
-    //options.setRestServer("120.26.4.73:81");
-    //options.setIMServer("120.26.4.73");
-    //options.setImPort(6717);
-
-    EaseUI.getInstance().init(this, null);
-    EMClient.getInstance().setDebugMode(true);
-    PreferenceManager.init(this);
-
-    EMClient.getInstance().addConnectionListener(new EMConnectionListener() {
-      @Override public void onConnected() {
-
-      }
-
-      @Override public void onDisconnected(int errorCode) {
-        if(errorCode == EMError.USER_LOGIN_ANOTHER_DEVICE)
-        {
-          Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          intent.putExtra("conflict", true);
-          startActivity(intent);
-        }
-      }
-    });
-  }
+    public static LiveApplication getInstance() {
+        return instance;
+    }
 
 }
