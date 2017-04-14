cf2c3aa611f4aa2b94643b2e8d69e4bdf5feb6a3
wangpengzhengwpz <2061743907@qq.com>
2017-04-14 17:39:14 +0800

    新建GiftDao，定义Gift的表名和字段名

---
+++ app/src/main/java/cn/ucai/live/data/dao/GiftDao.java
@@ -0,0 +1,13 @@
+package cn.ucai.live.data.dao;
+
+/**
+ * Created by w on 2017/4/14.
+ */
+
+public class GiftDao {
+    public static final String GIFT_TABLE_NAME = "t_superwechat_gift";
+    public static final String GIFT_COLUMN_ID = "m_gift_id";
+    public static final String GIFT_COLUMN_NAME = "m_giftr_name";
+    public static final String GIFT_COLUMN_URL = "m_gift_url";
+    public static final String GIFT_COLUMN_PRICE = "m_giftr_price";
+}
